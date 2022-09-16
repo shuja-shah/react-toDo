f9134ff426379ed65787484485a73e77f82c52fa
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-16 06:26:58 -0700

    Develop App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ListContainer from './Components/ListContainer';
+import Header from './Components/Header';
+
+const App = () => (
+  <>
+    <Header />
+    <ListContainer />
+  </>
+);
+export default App;
